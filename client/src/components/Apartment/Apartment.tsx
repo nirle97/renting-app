75d5a49f448d2169daf486e525e5eb1d29024fae
nirle97 <nirle97@gmail.com>
2021-06-21 16:24:31 +0300

    Update Apartment.tsx

--- client/src/components/Apartment/Apartment.tsx
+++ client/src/components/Apartment/Apartment.tsx
@@ -1,35 +1,46 @@
-import React, {useState} from "react";
-import "./apartment.css"
-import { IApt } from "../../interfaces/interface"
+import React, { useState } from "react";
+import "./apartment.css";
+import { IApt } from "../../interfaces/interface";
 import { useEffect } from "react";
 interface IProps {
-    apt: IApt 
-  }
-  
-function Apartment({apt}:IProps) {
-    const [picToDisplay, setPicToDisplay] = useState<number>(0)
+  apt: IApt;
+}
 
-   
-     
+function Apartment({ apt }: IProps) {
+  const [picToDisplay, setPicToDisplay] = useState<number>(0);
 
   return (
-  <div className="Apartment-container">
-    <div className="Apartment-top-section">
-        <img className="Apartment-img" src={apt.images[picToDisplay]}alt="apartment pics" />
-        
-        {picToDisplay !== apt.images.length -1  &&
-            <button className="Apartment-top-section-right-button" onClick={() => setPicToDisplay(prev => ++prev )}><i className="fas fa-arrow-right"></i></button>
-        }
-        {picToDisplay !== 0 &&
-            <button className="Apartment-top-section-left-button" onClick={() => setPicToDisplay(prev => --prev )}><i className="fas fa-arrow-left"></i></button>
-        }
-    </div>
-    <div className="Apartment-description">
+    <div className="Apartment-container">
+      <div className="Apartment-top-section">
+        <img
+          className="Apartment-img"
+          src={apt.images[picToDisplay]}
+          alt="apartment pics"
+        />
+
+        {picToDisplay !== apt.images.length - 1 && (
+          <button
+            className="Apartment-top-section-right-button"
+            onClick={() => setPicToDisplay((prev) => ++prev)}
+          >
+            <i className="fas fa-arrow-right"></i>
+          </button>
+        )}
+        {picToDisplay !== 0 && (
+          <button
+            className="Apartment-top-section-left-button"
+            onClick={() => setPicToDisplay((prev) => --prev)}
+          >
+            <i className="fas fa-arrow-left"></i>
+          </button>
+        )}
+      </div>
+      <div className="Apartment-description">
         <span>City:{apt.city}</span>
         <span>Price per month:{apt.pricePerMonth}</span>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
 export default Apartment;
